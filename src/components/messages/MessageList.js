f4c2b7f046084db63d634a95fa22b55fda686a51
Alex Martin <alex@subtleco.co>
2021-03-08 15:23:15 -0600

    User can now view and add messages

--- src/components/messages/MessageList.js
+++ src/components/messages/MessageList.js
@@ -1,15 +1,39 @@
-import React, { useContext, useEffect } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import { MessageCard } from './MessageCard'
 import { MessageContext } from './MessageProvider'
 import './MessageList.css'
 
 export const MessageList = () => {
-    const { messages, getMessages } = useContext(MessageContext)
+    const { messages, getMessages, addMessage } = useContext(MessageContext)
+    const [newText, setNewText] = useState("")
     const currentUserId = parseInt(sessionStorage.getItem("nutshell_user"))
+    let newMessageForm
 
     useEffect(() => {
         getMessages()
     }, [])
+
+    const handleChange = text => {
+        setNewText(text)
+    }
+
+    const handleSend = () => {
+        const newMessage = {
+            userId: currentUserId,
+            text: newText,
+            timestamp: Date.now()
+        }
+        addMessage(newMessage)
+        setNewText("")
+    }
+
+    if (currentUserId) {
+        newMessageForm =
+            <div className="newMessage">
+                <textarea onChange={e => handleChange(e.target.value)} className="newMessage__text" rows="4" cols="50" value={newText}></textarea>
+                <button className="button btn--create" onClick={handleSend}>Send</button>
+            </div>
+    }
 
     return (
         <section className="messages">
@@ -17,13 +41,10 @@ export const MessageList = () => {
                 messages.map(m => {
                     let currentUser = false
                     if (currentUserId === m.userId) currentUser = true
-                    return <MessageCard key={messages.id} message={m} currentUser={currentUser}/>
+                    return <MessageCard key={messages.id} message={m} currentUser={currentUser} />
                 })
             }
-            <div className="newMessage">
-                <textarea className="newMessage__text" rows="4" cols="50"></textarea>
-                <button className="btn--create">Send</button>
-            </div>
+            {newMessageForm}
         </section>
     )
 }
