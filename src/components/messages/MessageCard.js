30ec8bad05f9ffc32b6c91197fcf8f8e12f1dfc0
Alex Martin <alex@subtleco.co>
2021-03-08 14:42:57 -0600

    basic CSS styling for messages

--- src/components/messages/MessageCard.js
+++ src/components/messages/MessageCard.js
@@ -3,18 +3,19 @@
 //This module is responsible for rendering date from individual message objects to the DOM, and will only be called from MessageList.js
 
 import React from 'react'
+import './MessageCard.css'
 
 export const MessageCard = ({ message }) => {
     return (
-        <article className="message">
+        <article className="message message--card">
+            <div className="message_text">{message.text}</div>
             <div className="message__info">
-                <h4 className="message__info--userName">User ID: {message.userId}</h4>
+                <p className="message__info--userName">User ID: {message.userId}</p>
                 <p className="message__info--timestamp">{new Date(message.timestamp).toLocaleString('en-US')}</p>
             </div>
-            <div className="message_text">{message.text}</div>
-            <div className="message_buttons">
-                <button className="btn--edit">Edit</button>
-                <button className="btn--delete">Delete</button>
+            <div className="message__buttons">
+                <button className="button btn--edit">Edit</button>
+                <button className="button btn--delete">Delete</button>
             </div>
         </article>
     )
--- src/components/messages/MessageList.js
+++ src/components/messages/MessageList.js
@@ -1,6 +1,7 @@
 import React, { useContext, useEffect } from 'react'
 import { MessageCard } from './MessageCard'
 import { MessageContext } from './MessageProvider'
+import './MessageList.css'
 
 export const MessageList = () => {
     const { messages, getMessages } = useContext(MessageContext)
@@ -12,8 +13,12 @@ export const MessageList = () => {
     return (
         <section className="messages">
             {
-                 messages.map(m => <MessageCard key={messages.id} message={m}/>)
+                messages.map(m => <MessageCard key={messages.id} message={m} />)
             }
+            <div className="newMessage">
+                <textarea className="newMessage__text" rows="4" cols="50"></textarea>
+                <button className="btn--create">Send</button>
+            </div>
         </section>
     )
 }
