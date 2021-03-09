25769728bc060d32c56a321979cdd2f6861f1fb7
Stacey <stacey.littrell@gmail.com>
2021-03-09 12:29:52 -0600

    user can check off tasks as complete, completed tasks will not render to task list

--- src/components/tasks/Task.js
+++ src/components/tasks/Task.js
@@ -10,26 +10,37 @@ export const TaskCard = ({taskObj}) => {
 
     const handleChange = (event) => {
         const newTask = {...task}
-        newTask[event.target.id]=event.target.value
+        newTask[event.target.id]=event.target.checked
         setTask(newTask)
-        if(event.target.value === "true"){
+        // debugger
+        if(event.target.checked === true){
             const taskId = taskObj.id
-            const checked = task
+            const checked = newTask
+            updateTasks(taskId, checked)
+        }
+        if(event.target.checked === false) {
+            const taskId = taskObj.id
+            const checked = newTask
             updateTasks(taskId, checked)
         }
     }
+    
+    if(taskObj.complete === true){
+        return (<> </>)
+    }
+    else {
+    
+        return(
+            <>
+                <div>Complete by: {taskObj.completedByDate}</div>
+                <section>
+                    <input type= "checkbox" id="complete" onClick={handleChange}></input>
+                    <label forhtml={taskObj.id} className="task__name">
+                    <div>{taskObj.name}</div>
+                    </label>
+                </section>
+            </>
 
-
-    return(
-        <>
-            <div>Complete by: {taskObj.completedByDate}</div>
-            <section>
-                <input type= "checkbox" id="complete" value={true} onClick={handleChange}></input>
-                <label forhtml={taskObj.id} className="task__name">
-                <div>{taskObj.name}</div>
-                </label>
-            </section>
-        </>
-
-    )
+        )
+    }   
 }
