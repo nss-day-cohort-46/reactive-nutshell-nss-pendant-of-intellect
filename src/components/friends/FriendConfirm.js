51f8733ae8682d9b26f0010912e0412537565241
Dan Ross <daniellross@gmail.com>
2021-03-09 10:28:59 -0600

    got userById to display username on page for confirmation

--- src/components/friends/FriendConfirm.js
+++ src/components/friends/FriendConfirm.js
@@ -5,12 +5,32 @@
 
 import React, { useContext, useEffect, useState } from "react"
 import { useHistory, useParams } from 'react-router-dom';
+import { FriendsContext } from "./FriendsProvider"
+import { UsersContext } from "../users/UsersProvider"
 
 export const FriendConfirm = () => {
-    
+    const [user, setUser] = useState({})
+    const { addFriends } = useContext(FriendsContext)
+    const { getUserById } = useContext(UsersContext)
+    const { userId } = useParams()
+
+    useEffect(() => {
+        getUserById(userId)
+        .then((response) => {
+            setUser(response)
+        })
+    }, [])
+
+    const handleClickAddFriend = (event) => {
+        console.log(event.target)
+    }
+
     return (
         <>
-        Hello
+        Add {user.name} as a friend?
+
+        <button className="btn--save" onClick={handleClickAddFriend}>CONFIRM</button>
+        {/* <button className="btn--save" onClick={}>CANCEL</button> */}
         </>
     )
 }
