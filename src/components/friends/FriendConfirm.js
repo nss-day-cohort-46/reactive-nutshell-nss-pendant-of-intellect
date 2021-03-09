63c84f76275a131f4c89a6ddfcda7b560ef9198f
Dan Ross <daniellross@gmail.com>
2021-03-09 10:48:12 -0600

    added conditional to verify if user was lready friends or if user was adding themself as a friend

--- src/components/friends/FriendConfirm.js
+++ src/components/friends/FriendConfirm.js
@@ -4,33 +4,68 @@
  **/
 
 import React, { useContext, useEffect, useState } from "react"
-import { useHistory, useParams } from 'react-router-dom';
+import { useHistory, useParams, Link } from 'react-router-dom';
 import { FriendsContext } from "./FriendsProvider"
 import { UsersContext } from "../users/UsersProvider"
 
 export const FriendConfirm = () => {
     const [user, setUser] = useState({})
-    const { addFriends } = useContext(FriendsContext)
+    const { filteredFriends, getFriends, addFriends } = useContext(FriendsContext)
     const { getUserById } = useContext(UsersContext)
     const { userId } = useParams()
+    const currentUserId = parseInt(sessionStorage.nutshell_user)
 
     useEffect(() => {
         getUserById(userId)
-        .then((response) => {
-            setUser(response)
-        })
+            .then((response) => {
+                setUser(response)
+            })
     }, [])
 
     const handleClickAddFriend = (event) => {
         console.log(event.target)
     }
 
-    return (
-        <>
-        Add {user.name} as a friend?
+    // write a function that checks for:
+    // if currentUser and user are already friends
+    // if the user === currentUser
+    // provide different responses based on that
 
-        <button className="btn--save" onClick={handleClickAddFriend}>CONFIRM</button>
-        {/* <button className="btn--save" onClick={}>CANCEL</button> */}
-        </>
+    const friendConditional = () => {
+        const friendCheck = filteredFriends.find(f => f.userId === user.id)
+
+        if (currentUserId === user.id) {
+            return (
+                <>
+                    <div> You can't be friends with yourself!</div>
+                    <Link to="/messages"> Back to messages.</Link>
+                </>
+            )
+        } else if (friendCheck !== undefined) {
+            return (
+                <>
+                    <div>You are already friends with {user.name}.</div>
+                    <Link to="/messages"> Back to messages.</Link>
+                </>
+            )
+        }
+        else {
+            return (
+                <>
+                    Add {user.name} as a friend?
+
+                    <button className="btn--save" onClick={handleClickAddFriend}>CONFIRM</button>
+                    <Link to="/messages">
+                        <button className="btn--save">CANCEL</button>
+                    </Link>
+                </>
+            )
+        }
+    }
+
+
+
+    return (
+        friendConditional()
     )
 }
