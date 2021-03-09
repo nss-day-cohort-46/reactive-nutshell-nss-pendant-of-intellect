a89471d4877baec5600061a71647c9bcaec05276
Dan Ross <daniellross@gmail.com>
2021-03-09 10:17:13 -0600

    clicking username in chat routes to friendConfirm with userId available as aparam

---
+++ src/components/friends/FriendConfirm.js
@@ -0,0 +1,16 @@
+/** INFO
+ *  AUTHOR: Dan Ross
+ *  PURPOSE: This module confirms that a user wants to add another user as a friend
+ **/
+
+import React, { useContext, useEffect, useState } from "react"
+import { useHistory, useParams } from 'react-router-dom';
+
+export const FriendConfirm = () => {
+    
+    return (
+        <>
+        Hello
+        </>
+    )
+}
