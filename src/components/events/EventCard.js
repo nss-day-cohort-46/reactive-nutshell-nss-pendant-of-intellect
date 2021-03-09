7550372b668eb6fce52d9f2ffc031204b1417860
Alex Martin <alex@subtleco.co>
2021-03-09 15:08:27 -0600

    User can delete their events

--- src/components/events/EventCard.js
+++ src/components/events/EventCard.js
@@ -1,13 +1,23 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import './EventCard.css'
+import { EventContext } from './EventProvider'
 
 export const EventCard = ({ event }) => {
+    const { deleteEvent } = useContext(EventContext)
+
+    const handleClickDeleteButton = () => {
+        deleteEvent(event.id)
+    }
+
+
     return (
         <article className="event">
             <h2 className="event__name">{event.name}</h2>
             <p className="event__date">{event.date}</p>
             <p className="event__location">{event.city}, {event.state}</p>
             <button className="event__weatherButton button">Show Weather</button>
+            <button className="event__deleteButton button btn--delete"
+                onClick={handleClickDeleteButton}>Delete Event</button>
         </article>
     )
 }
