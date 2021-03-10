bbe2677c90879fc2d3fc0d6d15a9e06c1bed711f
kjk1325e <kaitlinjkelley@gmail.comm>
2021-03-10 14:10:54 -0600

    Added styling to article cards to meet acceptance criteria; added Read More button to take user to full article

--- src/components/newsArticle/NewsArticleCard.js
+++ src/components/newsArticle/NewsArticleCard.js
@@ -2,7 +2,7 @@
 // Build HTML for each article
 import React, { useContext } from "react"
 import { NewsArticleContext} from "./NewsArticleProvider"
-
+import "./NewsArticleCard.css"
 
 export const NewsArticleCard = ({article, currentUserId}) => {
     
@@ -15,15 +15,16 @@ export const NewsArticleCard = ({article, currentUserId}) => {
         <section className={parseInt(currentUserId )=== article.userId ? "currentUser" : "friend"}>
             <h2>{article.title}</h2>
             <div className="article__info">
-                <p>{article.user.name}</p>
-                <p>{new Date(article.timestamp).toLocaleString()}</p>
+                <p>Posted By {article.user.name} on {new Date(article.timestamp).toLocaleString().split(',')[0]}</p>
             </div>
-            <p>{article.synopsis}</p>
+            <p>Synopsis: {article.synopsis}</p>
             
+            <button className="btn--readMore"><a href={article.url} target="_blank">Read Full Article</a></button>
            {parseInt(sessionStorage.getItem("nutshell_user")) === article.userId ? 
-           
           <button className="btn--delete" onClick={event=> handleDeleteClick(event)}>Delete</button>: ""}
+          
             
         </section>
     )
 }
+
